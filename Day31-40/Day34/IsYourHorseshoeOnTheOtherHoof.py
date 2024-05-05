bb0814fb94cd2d7cc6ff7a545caffaa3e38d6256
Ram22an <118821139+Ram22an@users.noreply.github.com>
2024-05-05 19:43:59 +0530

    Day 34 A. Is your horseshoe on the other hoof?

---
+++ Day31-40/Day34/IsYourHorseshoeOnTheOtherHoof.py
@@ -0,0 +1,16 @@
+arr=list(map(int,input().split()))
+arr1=set(arr)
+print(4-len(arr1))
+
+
+
+# this is code with array implementation i have submitted both and both are working
+# arr=list(map(int,input().split()))
+# arr1=[]
+# for i in range(4):
+#     if arr[i] not in arr1:
+#         arr1.append(arr[i])
+# print(4-len(arr1))
+
+# here i got to know about set it does not let any duplicate value  
+    
