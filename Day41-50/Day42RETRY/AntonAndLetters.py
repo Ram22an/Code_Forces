47f1affaf476fc536f4fdb1a92fe1a85682fd73b
Ram22an <118821139+Ram22an@users.noreply.github.com>
2024-05-13 17:48:05 +0530

    I will try again this Day 42
    i am having paper right now

---
+++ Day41-50/Day42RETRY/AntonAndLetters.py
@@ -0,0 +1,9 @@
+StringList=list(map(str,input().strip().split(",")))
+if len(StringList)>2:
+    StringList[0]=StringList[0][1:]
+    StringList[-1]=StringList[-1][:-1]
+    StringList2=[element.strip() for element in StringList]
+    Dir1=set(StringList2)
+    print(len(Dir1))
+else:
+    print(0)
