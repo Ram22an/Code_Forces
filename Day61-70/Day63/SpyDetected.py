19eb66f9ce503d5feea8746b0054dfbb4f4c3305
Ram22an <118821139+Ram22an@users.noreply.github.com>
2024-06-23 21:59:17 +0530

    Brute Force of A. Spy Detected!

--- Day61-70/Day63/SpyDetected.py
+++ Day61-70/Day63/SpyDetected.py
@@ -8,3 +8,23 @@ for i in range(x):
             Value=i
             break
     print(Arry.index(Value)+1)
+# here is the brute force method without count 
+FirstCount=1
+FirstIndex=0
+FirstVal=Arry[0]
+SecondCount=0
+SecondIndex=0
+SecondVal=0
+for i in range(1,Size):
+    if Arry[i]==FirstVal:
+        FirstCount+=1
+        FirstIndex=i
+    else:
+        SecondCount+=1
+        SecondIndex=i
+        SecondVal=Arry[i]
+if SecondCount>1:
+    Value=FirstIndex+1
+else:
+    Value=SecondIndex+1
+print(Value)
