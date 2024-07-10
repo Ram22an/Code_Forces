774a375177f888b3d56a5588d9cf114c8509a637
Ram22an <118821139+Ram22an@users.noreply.github.com>
2024-07-10 17:47:52 +0530

    A. Black Square

---
+++ Day71-80(800)/Day80/BlackSquare.py
@@ -0,0 +1,9 @@
+Carolies=[0]
+Carolies1=list(map(int,input().split(" ")))
+Carolies+=Carolies1
+StripStr=input()
+Strips=list(map(int,StripStr))
+Total=0
+for i in range(len(Strips)):
+    Total+=Carolies[Strips[i]]
+print(Total)
